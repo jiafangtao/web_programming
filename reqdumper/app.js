5350bfd9f6c81ac8dc1ec7e86a1b1e5c38644fd2
Bruce Jia <bruce.jia@autodesk.com>
2018-06-20 16:34:46 +0800

    Dump header Content-Length

--- reqdumper/app.js
+++ reqdumper/app.js
@@ -4,13 +4,20 @@ var app = express();
 
 app.use(function (req, resp) {
     var content = '';
+
     var ua = req.get('User-Agent');
     content += 'User-Agent: ' + ua;
-    resp.send(content);
 
+    var clen = req.get('Content-Length');
+
+    if (clen != null)
+        content += '\nContent-Length: ' + clen;
+
+    console.log(content);
+    resp.send(content);
 });
 
 app.listen(3000);
 
-console.log ("Listening on port 3000");
+console.log ("Listening on port 3000 ......");
 
