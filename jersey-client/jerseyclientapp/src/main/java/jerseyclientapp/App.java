f11b30a48753cee32c7b76501d765406176ea6ce
Bruce Jia <bruce.jia@autodesk.com>
2018-06-22 16:17:14 +0800

    Add a test program for jersey client

---
+++ jersey-client/jerseyclientapp/src/main/java/jerseyclientapp/App.java
@@ -0,0 +1,54 @@
+package jerseyclientapp;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		log ("Posting with Header: Content-Length: 0 ......");
+		doPost("http://localhost:3000", null);
+		
+		log ("Done.");
+	}
+	
+	public static void doPost (String url, String data) {
+		
+		Response response = null;
+    	
+		Client client = ClientBuilder.newBuilder().build();
+        WebTarget webTarget = client.target(url).queryParam("responseFormat", "json");
+        
+        Invocation.Builder invocBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
+        
+        if (data == null) {
+        	response = invocBuilder.header("Content-Length", 0).post(null);
+        }
+        else
+        {
+        	Entity<String> entity = Entity.entity(data, MediaType.TEXT_PLAIN_TYPE);
+        	response = invocBuilder.post(entity);
+        }
+                
+        int code = response.getStatus();
+        
+        if (code >= 200 && code < 300) {
+        	
+        	String result = response.readEntity(String.class);
+        	log(String.format("Response: %s", result));
+        }
+        else {
+        	log(String.format("Something went wrong with status %s", code));
+        }
+	}
+	
+	public static void log (String message) {
+		System.out.println(message);
+	}
+}
