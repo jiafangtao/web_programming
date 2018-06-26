3b17ccae20ecd9dc17a4aa2e0c5b9a9e4c1cb7f2
Bruce Jia <bruce.jia@autodesk.com>
2018-06-26 17:17:33 +0800

    Fit and finish Jersey client

--- jersey-client/jerseyclientapp/src/main/java/jerseyclientapp/App.java
+++ jersey-client/jerseyclientapp/src/main/java/jerseyclientapp/App.java
@@ -8,9 +8,15 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.RequestEntityProcessing;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
 		
 		log ("Posting with Header: Content-Length: 0 ......");
 		doPost("http://localhost:3000", null);
@@ -26,15 +32,25 @@ public class App {
 		
 		Response response = null;
     	
-		//Client client = ClientBuilder.newBuilder().build();
-		Client client = ClientBuilder.newClient();
+		
+		ClientConfig config = new ClientConfig();
+		config.register(CheckRequestFilter.class);
+		
+		config.property(ClientProperties.REQUEST_ENTITY_PROCESSING, RequestEntityProcessing.BUFFERED);
+		
+		Client client = ClientBuilder.newBuilder().build();
+		//Client client = ClientBuilder.newClient(config);
+		
+		//Client client = ClientBuilder.newClient().register(CheckRequestFilter.class);
+		
 		
         WebTarget webTarget = client.target(url).queryParam("responseFormat", "json");
         
         Invocation.Builder invocBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
         
         if (data == null) {
-        	response = invocBuilder.header("Content-Length", 0).post(null);
+        	response = invocBuilder.header("content-length", 0).post(null);
+        	//response = invocBuilder.post(null);
         }
         else
         {
