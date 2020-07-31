a991a76cee9c9d23f0df80e4b50dfcca5ae8862a
Bruce Jia <bruce.jia@autodesk.com>
2020-07-31 18:34:19 +0800

    Some basic scripts playing with es6

---
+++ es6/arrowfunctions.js
@@ -0,0 +1,7 @@
+// naming an anoymous function
+let f1 = (greetings) => { console.log("hello, " + greetings); }
+f1("javascript");
+
+((greetings) => { 
+    console.log("hello, " + greetings); 
+})("EcmaScript 2015");
