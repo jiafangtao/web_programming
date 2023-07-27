7463ffcb74c2bda2977d001d5e3000a054f63d62
Bruce Jia <jiafangtao@gmail.com>
2023-07-27 21:58:47 +0800

    play neo4j with python driver

---
+++ neo4j.demo/README.md
@@ -0,0 +1,4 @@
+docker pull neo4j:4.4.23-community
+
+pip install neo4j==5.10.0
+
---
+++ neo4j.demo/start.bat
@@ -0,0 +1,5 @@
+docker run -d -p 7687:7687 neo4j:4.4.23-community
+python main.py
+
+
+https://pypi.org/project/neo4j/5.10.0/
