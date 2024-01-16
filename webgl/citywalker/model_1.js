af9834537a318a1c94a8313bbd8307bb85563605
Bruce Jia <jiafangtao@gmail.com>
2024-01-16 14:25:06 +0800

    more for citywalker

--- webgl/citywalker/model_1.js
+++ webgl/citywalker/model_1.js
@@ -2,16 +2,16 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000000 );
+const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 10 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const axesHelper = new THREE.AxesHelper( 10 );
+const axesHelper = new THREE.AxesHelper( .1 );
 scene.add( axesHelper );
 
-camera.position.z = 100;
+camera.position.z = .1;
 
 
 const loader = new GLTFLoader();
@@ -19,9 +19,15 @@ const loader = new GLTFLoader();
 loader.load( 'static/models/BarramundiFish.glb', function ( gltf ) {
 
 	//scene.add( gltf.scene );
-	scene.add(gltf.scene)
+	let model = gltf.scene;
+
+	scene.add(model);
 	console.info("The model was loaded successfully");
-	console.log(gltf.scene);
+	
+	console.log("Scene - ", model);
+	console.log("Scenes - ", gltf.scenes);
+
+	model.scale.set(10, 10, 10);
 
 }, undefined, function ( error ) {
 
@@ -29,7 +35,11 @@ loader.load( 'static/models/BarramundiFish.glb', function ( gltf ) {
 
 } );
 
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+const light = new THREE.AmbientLight( 0x808080 ); // soft white light
 scene.add( light );
+
+const hemiLight = new THREE.HemisphereLight(0x0000ff, 0x00ff00, 0.6);
+hemiLight.position.set(0, 100, 0);
+scene.add(hemiLight);
 
 renderer.render(scene, camera);
