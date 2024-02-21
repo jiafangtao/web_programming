f3ca0ba8aea43fc7e1af3708797c72ee6c4aa8e6
Bruce Jia <jiafangtao@gmail.com>
2024-02-21 17:23:27 +0800

    [rust tutorial] refine c12 with iterator

--- rust-tutorial/c12/src/main.rs
+++ rust-tutorial/c12/src/main.rs
@@ -1,45 +1,16 @@
 use std::env;
-use std::fs;
 use std::process;
-
-
-// str vs String
-//
+use c12::Config;
 
 
 fn main() {
-
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     
-
-    println!("(info) We've got arguments - {}, {}", config.query, config.file_path);
-    let contents = fs::read_to_string(config.file_path).expect("A valid text file is expected");   
-    
-    println!("{}", contents);
-}
-
-//TODO: the parameter type can be &[String] or &Vec<String>,
-//  is there any difference?
-//
-
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config {query, file_path})
+    if let Err(e) = c12::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
 }
