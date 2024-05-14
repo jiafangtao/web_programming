5773d609df2d18100d200748a42e07ceeb749242
Bruce Jia <jiafangtao@gmail.com>
2024-05-14 11:02:29 +0800

    cmake snippets following the tut

---
+++ cmake-tut/step01/tutorial.cpp
@@ -0,0 +1,8 @@
+#include "TutorialConfig.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Version: ";
+    std::cout << _VERSION_MAJOR << "." << _VERSION_MINOR << std::endl;
+    return 0;
+}
